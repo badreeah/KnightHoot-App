4c664b66650b96f0aef0bbf8e25e294b0c15741e
badreeah <148608538+badreeah@users.noreply.github.com>
2025-11-08 18:35:02 +0300

    Revert "Resolved merge conflicts and updated Statistics screenFeature/alerts stats"

--- supabase.js
+++ supabase.js
@@ -1,10 +1,19 @@
-import "react-native-url-polyfill/auto"; 
-import AsyncStorage from "@react-native-async-storage/async-storage"; 
-import { createClient, processLock } from "@supabase/supabase-js"; 
-// رابط المشروع (Project URL) 
-const SUPABASE_URL = "https://qsgrxnzljtoebmeqcpbp.supabase.co"; 
-//  المفتاح العام (Anon Public Key) 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzZ3J4bnpsanRvZWJtZXFjcGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MzQ1MTMsImV4cCI6MjA3NDIxMDUxM30.2sHDLxRF_dZp0tbZ5_Pefed3rsOoEfw5zMVAjEjIqZs"; 
-export const supabase = createClient( SUPABASE_URL, SUPABASE_ANON_KEY, { auth: { storage: AsyncStorage, autoRefreshToken: true, persistSession: true, detectSessionInUrl: false, lock: processLock,
-  
- }, } ); export default supabase;
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient, processLock } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+      lock: processLock,
+    },
+  }
+);
+
+export default supabase;
