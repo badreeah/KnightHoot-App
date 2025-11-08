eca979734b70b44ccfa06a80fb43981bb0befaa5
Alreem Alothman <alreemothman66@gmail.com>
2025-11-08 11:47:42 +0300

    Revert "Merge branch 'main' into Profile-Backendd"
    This reverts commit a751fc5eab7fa31aa39e2e0a8a0d94357b60ad34, reversing
    changes made to d11d9e570772b2e021a32b4f52ee43dec731fa72.

--- Screens/Home.js
+++ Screens/Home.js
@@ -127,7 +127,7 @@ export default function Home({ navigation }) {
             iconWidth={120}
             iconHeight={120}
             top={-18}
-            onPress={() => console.log("Scan URL")}
+            onPress={() => navigation.navigate("ScanURL")}
           />
           <ActionButton
             title="Report Scam"
@@ -136,8 +136,7 @@ export default function Home({ navigation }) {
             iconWidth={114}
             top={-30}
             iconHeight={114}
-            onPress={() => navigation.navigate('ReportScam')}
-
+            onPress={() => navigation.navigate("ReportScam")}
           />
         </ScrollView>
       </View>
